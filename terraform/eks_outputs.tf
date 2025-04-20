62f76be8bd20110d70f31a749b09accf6aa0473c
Abdo <Abdo@hotmail.com>
2025-04-21 00:08:57 +0200

    adding

--- terraform/eks_outputs.tf
+++ terraform/eks_outputs.tf
@@ -1,15 +1,15 @@
-#output "eks_cluster_name" {
-#  value = aws_eks_cluster.cluster.name
-#}
-#
-#output "eks_cluster_endpoint" {
-#  value = aws_eks_cluster.cluster.endpoint
-#}
-#
-#output "eks_cluster_kubeconfig" {
-#  value = aws_eks_cluster.cluster.kubeconfig[0].value
-#}
-#
-#output "eks_cluster_ca_certificate" {
-#  value = aws_eks_cluster.cluster.certificate_authority[0].data
-#}
+output "eks_cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.cluster.endpoint
+}
+
+output "eks_cluster_kubeconfig" {
+  value = aws_eks_cluster.cluster.kubeconfig[0].value
+}
+
+output "eks_cluster_ca_certificate" {
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
+}
