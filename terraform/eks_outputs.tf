f74404bfc2f41cdaa41e7bb085ba19ff1d5a5a34
Abdo <Abdo@hotmail.com>
2025-04-27 15:52:35 +0300

    finalize the project

--- terraform/eks_outputs.tf
+++ terraform/eks_outputs.tf
@@ -1,34 +1,34 @@
-#output "eks_cluster_name" {
-#  value = aws_eks_cluster.eks_cluster.name
-#}
-#
-#output "eks_cluster_endpoint" {
-#  value = aws_eks_cluster.eks_cluster.endpoint
-#}
-#
-#output "eks_cluster_ca_certificate" {
-#  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-#}
-#
-#output "eks_cluster_kubeconfig" {
-#  value = <<EOF
-#apiVersion: v1
-#clusters:
-#- cluster:
-#    certificate-authority-data: ${aws_eks_cluster.eks_cluster.certificate_authority[0].data}
-#    server: ${aws_eks_cluster.eks_cluster.endpoint}
-#  name: ${aws_eks_cluster.eks_cluster.name}
-#contexts:
-#- context:
-#    cluster: ${aws_eks_cluster.eks_cluster.name}
-#    user: aws
-#  name: ${aws_eks_cluster.eks_cluster.name}
-#current-context: ${aws_eks_cluster.eks_cluster.name}
-#kind: Config
-#users:
-#- name: aws
-#  user:
-#    token: ${data.aws_eks_cluster_auth.cluster_auth.token}
-#EOF
-#  sensitive = true
-#}
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_ca_certificate" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "eks_cluster_kubeconfig" {
+  value = <<EOF
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${aws_eks_cluster.eks_cluster.certificate_authority[0].data}
+    server: ${aws_eks_cluster.eks_cluster.endpoint}
+  name: ${aws_eks_cluster.eks_cluster.name}
+contexts:
+- context:
+    cluster: ${aws_eks_cluster.eks_cluster.name}
+    user: aws
+  name: ${aws_eks_cluster.eks_cluster.name}
+current-context: ${aws_eks_cluster.eks_cluster.name}
+kind: Config
+users:
+- name: aws
+  user:
+    token: ${data.aws_eks_cluster_auth.cluster_auth.token}
+EOF
+  sensitive = true
+}
--- terraform/s3_backend.tf
+++ terraform/s3_backend.tf
@@ -1,40 +1,40 @@
-#resource "aws_s3_bucket" "terraform_state" {
-#  bucket = "devops-project-tfstate-salah-2025"
-#
-#  tags = {
-#    Name        = "Terraform State Bucket"
-#    Environment = "devops"
-#  }
-#}
-#
-#resource "aws_s3_bucket_versioning" "enabled" {
-#  bucket = aws_s3_bucket.terraform_state.id
-#
-#  versioning_configuration {
-#    status = "Enabled"
-#  }
-#}
-#
-#resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-#  bucket = aws_s3_bucket.terraform_state.id
-#
-#  rule {
-#    apply_server_side_encryption_by_default {
-#      sse_algorithm = "AES256"
-#    }
-#  }
-#}
-#
-#resource "aws_dynamodb_table" "terraform_locks" {
-#  name           = "terraform-locks"
-#  billing_mode   = "PAY_PER_REQUEST"
-##  read_capacity  = 5
-##  write_capacity = 5
-#  hash_key       = "LockID"
-#
-#  attribute {
-#    name = "LockID"
-#    type = "S"
-#  }
-#
-#}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "devops-project-tfstate-salah-2025"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "devops"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "terraform-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+#  read_capacity  = 5
+#  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
